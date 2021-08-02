99f294991ccc3a414bfdbc31e8daec310e397cd4
yue <15929308055@163.com>
2021-08-02 21:52:39 +0800

    创建了main.cpp，并写了初始代码

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+void sort(int* arr, int size)
+{
+
+}
+
+int main()
+{
+	int arr[] = {12,1,34,55,67,89,44};
+	int size = sizeof(arr) / sizeof(arr[0]);
+	sort(arr, size);
+	return 0;
+}
